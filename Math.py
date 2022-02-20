17167f91759a235cab03eb0d6d69a21d5ff2ca63
akanshavatsa <99544750+akanshavatsa@users.noreply.github.com>
2022-02-20 20:44:32 +0530

    commit of math.py

---
+++ Math.py
@@ -0,0 +1,27 @@
+#add implementation
+def add(x,y):
+    return(x+y)
+#sub implementation	
+def subtract(x,y):
+
+    #bug456
+
+#multiply implementation	
+def multiply(x,y):
+    return (x*y) #bug456
+#multiply implementation	
+def multiply(x,y):
+    return(x*y)        #master branch
+
+#divide implementation	
+def divide(x,y):
+
+    return (x/y)    #master branch
+
+    if y==0:           # on bug 789
+		return DIVIDE_BY_0_ERROR
+	else
+	return x/y;
+
+ 
+
